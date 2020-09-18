acce6057db3a7dbce2198939c8a58e0a1dad08cc
rupesh2020 <rupesh2020@bhaskars-MacBook-Pro.local>
2020-09-18 10:37:19 +0530

    division of two numbers

---
+++ shCodes/division.sh
@@ -0,0 +1,17 @@
+#
+#
+#=>A program to substract two numbers
+#
+#
+echo "Enter two numbers: "
+
+echo "Enter first number: "
+read num1
+echo "Enter second number:"
+read num2
+
+
+Total=$((num1 % num2))
+
+echo "Result = $Total"
+
