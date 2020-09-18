7dbc566cf751feb7056e0021aed02a3bc67c755e
rupesh2020 <rupesh2020@bhaskars-MacBook-Pro.local>
2020-09-18 10:23:55 +0530

    two numbers multiply

---
+++ shCodes/multiply.sh
@@ -0,0 +1,20 @@
+#
+#A program to multiply two numbers
+#
+#
+
+
+
+echo "Enter first number: "
+read num1
+echo "Enter second number: "
+read num2
+
+
+c=$((num1 * num2))
+
+
+echo "Result = $c"
+
+
+
