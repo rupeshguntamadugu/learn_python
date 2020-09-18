1dd8f40030ff021792e8268f595aa7f9b7bfe9fd
rupesh2020 <rupesh2020@bhaskars-MacBook-Pro.local>
2020-09-18 10:43:19 +0530

    substraction of two num

---
+++ shCodes/substraction.sh
@@ -0,0 +1,14 @@
+#
+#A program to substraction of two numbers
+#==>
+
+echo "Enter first number: "
+read num1
+echo "Enter second number: "
+read num2
+
+
+Total=$((num1 - num2))
+
+echo "Result = $Total"
+
