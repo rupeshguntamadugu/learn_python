2ac750eab3d9f4be6eb809d5aa704d852738f528
rupesh2020 <guntamadugurupesh@gmail.com>
2020-09-22 20:10:39 +0530

    givencharacter is vowel or not

---
+++ shCodes/15_vowel_or_not.sh
@@ -0,0 +1,14 @@
+#
+#A program to check whether the entered character is vowel or not.
+
+echo "enter a character: "
+read a
+case $a in
+a)echo "it is a vowel";;
+e)echo "it is a vowel";;
+i)echo "it is a vowel";;
+o)echo "it is a vowel";;
+u)echo "it is a vowel";;
+*)echo "Not a vowel";;
+esac
+
