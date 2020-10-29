872445c18330a5e448a71666e0e362cadf1df84b
Rupesh <guntamadugurupesh@gmail.com>
2020-10-29 19:38:29 +0530

    Create array.bat

---
+++ batchCodes/array.bat
@@ -0,0 +1,26 @@
+@echo off
+ 
+set fruits[0]=Apple
+set fruits[1]=Apricot
+set fruits[2]=Asparagus
+set fruits[3]=Aubergine
+set fruits[4]=Banana
+  
+ 
+set /A i = 0
+ 
+:my_loop
+    
+    if defined fruits[%i%]  (
+     
+        call echo element at %i% = %%fruits[%i%]%%  
+         
+        set /a i = %i% + 1
+         
+        goto :my_loop
+     
+    )  
+ 
+echo done!
+ 
+pause
