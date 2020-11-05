2d718141fd5888bac5be8127cf8b1b2c0ffb40ab
Rupesh <guntamadugurupesh@gmail.com>
2020-11-05 12:15:24 +0530

    Create mathematical.bat

---
+++ batchCodes/mathematical.bat
@@ -0,0 +1,22 @@
+@echo off
+
+echo These are mathematical expressions
+
+set /a sum=15+16
+echo %sum%
+
+set /a mul=56*35
+echo %mul%
+
+set /a div=9/3
+echo %div%
+
+set /a assign=10
+echo %assign%
+
+set /a assign+=15
+echo %assign%
+
+set /a mod=10%3
+echo %mod%
+pause
